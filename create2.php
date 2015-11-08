900569f38fc2f087dd705052c9fefadf42faa56b
Eric Zhang <ezhang2008@gmail.com>
2015-11-08 04:33:49 -0500

    real2

---
+++ create2.php
@@ -0,0 +1,31 @@
+<html>
+<?php
+$servername = "us-cdbr-azure-east-a.cloudapp.net";
+$username = "ba1f432f661a46";
+$password = "75ac9de4";
+$dbname = "YHacks";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "CREATE TABLE Items
+(
+ItemID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+Price int,
+Name varchar(255),
+Description varchar(510),
+Link varchar(255)
+)";
+if ($conn->query($sql) === TRUE){
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
+</html>
