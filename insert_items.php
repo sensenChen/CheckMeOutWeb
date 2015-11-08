2a7f7c6ad512ce541aafe96eb7b66eca9947c1da
Sensen <chens16@rpi.edu>
2015-11-08 04:55:03 -0500

    added form and post operations

--- insert_items.php
+++ insert_items.php
@@ -17,7 +17,7 @@
     $LockID = $_POST['LockID'];
     $ItemID = $_POST['ItemID'];
 
-    $sql = "INSERT INTO Items(LockID, ItemID) VALUES([$LockID], [$ItemID])";
+    $sql = "INSERT INTO Items(LockID, ItemID) VALUES($LockID, $ItemID)";
     $result = mysql_query($sql);
 
     if(!$result){
