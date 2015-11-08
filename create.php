b0ed7ffb08c4ea8ea2272d6a0cdb56e461eced72
Eric Zhang <ezhang2008@gmail.com>
2015-11-08 04:22:00 -0500

    im dumb

--- create.php
+++ create.php
@@ -6,7 +6,7 @@ $password = "75ac9de4";
 $dbname = "YHacks";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
