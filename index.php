9bc7c6119c213a92b2ca6e41711208973c2c7483
Sensen <chens16@rpi.edu>
2015-11-08 03:21:01 -0500

    it works

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-    $link = mysql_connect('checkmeout.azurewebsites.net', 'CheckMeOutHacks', 'YHack2015');
-    if (!$link) {
-        die('Could not connect: ' . mysql_error());
-    }
-    echo 'Connected successfully';
-    mysql_close($link);
+    // $link = mysql_connect('checkmeout.azurewebsites.net', 'CheckMeOutHacks', 'YHack2015');
+    // if (!$link) {
+    //     die('Could not connect: ' . mysql_error());
+    // }
+    // echo 'Connected successfully';
+    // mysql_close($link);
 ?>
