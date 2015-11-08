3ac0bfde3545c34114a1f6c33e86a650316a8a86
Eric Zhang <ezhang2008@gmail.com>
2015-11-08 06:43:41 -0500

    lock

---
+++ unlocked.php
@@ -0,0 +1,25 @@
+<html>
+<?php
+$servername = "us-cdbr-azure-east-a.cloudapp.net";
+$username = "ba1f432f661a46";
+$password = "75ac9de4";
+$dbname = "YHacks";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Unlocked";
+$result = $conn->query($sql);
+
+$rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+    print $r['LockID']."<br>";
+}
+
+$conn->close();
+?>
+</html>
