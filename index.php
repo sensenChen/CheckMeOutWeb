73db475059dc428e6841c8f67fb8d218eed43a3d
Sensen <chens16@rpi.edu>
2015-11-08 04:08:00 -0500

    it works

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<html>
-hello
-</html>
+$sql1 = "CREATE TABLE Inventory (LockID int,ItemID int);";
+$sql2 = "CREATE TABLE Items (ItemID int, StoreID int, ItemName varchar(50), ItemDescription varchar(50), ItemPrice float, PictureLink varchar(50));";
+
+<?php
+    $result = mysql_query($sql1);
+
+    if(!$result){
+
+        die("did not work");
+    }
+
+    echo "did work";
+ ?>
