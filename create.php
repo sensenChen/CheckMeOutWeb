dc77232e8ddc7ff49fce672e8f38cac570ac0214
Eric Zhang <ezhang2008@gmail.com>
2015-11-08 03:34:38 -0500

    fixed create

--- create.php
+++ create.php
@@ -3,9 +3,10 @@
 $servername = "us-cdbr-azure-east-a.cloudapp.net";
 $username = "b94754eb2c3c77";
 $password = "e2eb2fa2";
+$db = "CheckMeOutHacks";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
