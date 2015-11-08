edfb2775b95096b4eecc809c893b61961aa20eb4
Sensen <chens16@rpi.edu>
2015-11-08 04:41:32 -0500

    added form and post operations

---
+++ form.php
@@ -0,0 +1,21 @@
+<html>
+    <form action="input_inventory.php" method="POST">
+        <em>Inventory-Lock Form</em>
+        Lock ID: <input type="number" value="LockID">
+        Item ID: <input type="number" value="ItemID">
+        <input type="submit">
+    </form>
+
+
+    <form action="input_items.php" method="POST">
+        <em>Item Input Form</em>
+        Lock ID: <input type="number" value="LockID">
+        Store ID: <input type="number" value="StoreID">
+
+        Item Name: <input type="text" value="ItemName">
+        Item Description: <input type="text" value="ItemDescription">
+        Item Cost: <input type="number" value="ItemCost">
+        Item Image Link: <input type="text" value="ItemImage">
+        <input type="submit">
+    </form>
+</html>
---
+++ insert_inventory.php
@@ -0,0 +1,32 @@
+<?php
+    $servername = "us-cdbr-azure-east-a.cloudapp.net";
+    $username = "ba1f432f661a46";
+    $password = "75ac9de4";
+    $dbname = "YHacks";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else {
+        echo "did connect";
+    }
+
+    $LockID = $_POST['LockID'];
+    $ItemID = $_POST['ItemID'];
+
+    $sql = "INSERT INTO INVENTORY(LockID, ItemID) VALUES([$LockID], [$ItemID])"
+    $result = mysql_query($sql1);
+
+    if(!$result){
+
+        die("did not work");
+    }
+
+    echo "did work";
+
+
+    $conn->close();
+?>
