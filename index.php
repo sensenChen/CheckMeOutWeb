d5fd1cbe74f403179e9dcb275fb04b80aff24248
Sensen <chens16@rpi.edu>
2015-11-08 04:11:20 -0500

    asdas

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
-$sql1 = "CREATE TABLE Inventory (LockID int,ItemID int);";
-$sql2 = "CREATE TABLE Items (ItemID int, StoreID int, ItemName varchar(50), ItemDescription varchar(50), ItemPrice float, PictureLink varchar(50));";
 
 <?php
+    $servername = "us-cdbr-azure-east-a.cloudapp.net";
+    $username = "ba1f432f661a46";
+    $password = "75ac9de4";
+    $dbname = "YHacks";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+
+    $sql1 = "CREATE TABLE Inventory (LockID int,ItemID int);";
+    $sql2 = "CREATE TABLE Items (ItemID int, StoreID int, ItemName varchar(50), ItemDescription varchar(50), ItemPrice float, PictureLink varchar(50));";
+
+
     $result = mysql_query($sql1);
 
     if(!$result){
@@ -10,4 +26,7 @@ $sql2 = "CREATE TABLE Items (ItemID int, StoreID int, ItemName varchar(50), Item
     }
 
     echo "did work";
+
+
+    $conn->close();
  ?>
