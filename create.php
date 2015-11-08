6430cb41f41f4e54ff655e49bac5a815bb4bf828
Eric Zhang <ezhang2008@gmail.com>
2015-11-08 03:33:03 -0500

    fixed create

--- create.php
+++ create.php
@@ -3,23 +3,20 @@
 $servername = "us-cdbr-azure-east-a.cloudapp.net";
 $username = "b94754eb2c3c77";
 $password = "e2eb2fa2";
-$dbname = "CheckMeOutHacks";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "CREATE TABLE Items(
-ItemID int,
-StoreID int,
-Name varchar(255),
-Description varchar(255),
-Link varchar(255)
-);"
-$result = $conn->query($sql);
+$sql = "CREATE TABLE Items";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
 
 $conn->close();
 ?>
