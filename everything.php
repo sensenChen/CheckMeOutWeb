75e3cd98a75ddd030629551c04e9075c5ccb2dab
Eric Zhang <ezhang2008@gmail.com>
2015-11-08 05:26:51 -0500

    more error checking

--- everything.php
+++ everything.php
@@ -10,16 +10,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT * FROM Inventory";
 $result = $conn->query($sql);
 
 $rows = array();
 while($r = mysqli_fetch_assoc($result)) {
-    $rows[] = $r;
+    print $r['LockID'].','.$r['ItemID']."\n";
 }
-print json_encode($rows);
 
 $conn->close();
 ?>
